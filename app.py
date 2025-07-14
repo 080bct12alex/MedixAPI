344c429ded45ad66d4df4c895fab8d6572dc72e3
ALEX SHRESTHA <080bct012.alex@pcampus.edu.np>
2025-07-14 09:28:13 +0545

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.add_middleware(  
+app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://medixapi.onrender.com"],
+    allow_origins=["*"],  # allow all origins temporarily
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
